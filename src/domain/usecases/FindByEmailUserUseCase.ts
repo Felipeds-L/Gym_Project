7c3bec6d1fd6c34402e48d244e398f662ff09569
Felipe Leite <felipesd.leite@gmail.com>
2026-05-29 01:01:32 -0300

    feat: adding all findBy usecases

---
+++ src/domain/usecases/FindByEmailUserUseCase.ts
@@ -0,0 +1,14 @@
+import User from "../entities/User";
+import {IUserRepository} from "../repositories/IUserRepository";
+
+export class findByIDUserUseCase {
+    constructor(private userRepository: IUserRepository) {}
+    
+    async execute(id: string): Promise<User> {
+        const user = await this.userRepository.findById(id);
+        if (!user) {
+            throw new Error("User not found");
+        }
+        return user;
+    }
+}
